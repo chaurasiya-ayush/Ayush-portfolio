1a4b3012a0bec3440288c6bfe259a1a56cf15cc8
Ayush-Chaurasiya <ayushchaurasiya9532951470@gmail.com>
2024-12-25 15:58:01 +0530

    first commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ box.addEventListener("click",() =>{
 document.getElementById("show").innerText ="DBL click to show less";
 document.querySelector("#photo1").style.display = "unset";
 document.querySelector("#photo2").style.display = "unset";
+document.querySelector("#photo3").style.display = "unset";
 // document.querySelector("#show").style.border = "orange";
 console.log("clicked");
 });
@@ -11,6 +12,7 @@ box.addEventListener("dblclick",() =>{
 document.getElementById("show").innerText ="Show more";
 document.querySelector("#photo1").style.display = "none";
 document.querySelector("#photo2").style.display = "none";
+document.querySelector("#photo3").style.display ="none";
 document.querySelector("#show").style.backgroundColor = "#8a9998";
 console.log("clicked");
 })
